41e10bc6efbe5502dbba0514fb2dc766013f41a6
Somesh Kar <somesh.kar@gmail.com>
2022-12-20 18:05:41 +0530

    Responsive navbar + reg box (#6)
    * Make responsive nav work to some extent
    
    * Make email field responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        phone: { max: "768px" },
+        laptop: { min: "768px" },
+      },
+    },
   },
   plugins: [],
 };
