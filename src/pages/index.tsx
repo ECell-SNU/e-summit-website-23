70bf3d05262fb3a205af3078d27b1d5b1befb79e
Rishi Balamurugan <45452035+Crazzygamerr@users.noreply.github.com>
2023-01-14 14:38:48 +0530

    update: Home page UI changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -109,15 +109,15 @@ const Home: NextPage = () => {
 	}, [controls]);
 
 	return (
-		<div className="flex w-full flex-col items-center">
-			<div className="relative flex w-full items-center justify-start pt-[30vh] lg:pt-[20vh]">
+		<div className="flex w-full flex-col items-center mt-[70px]">
+			<div className="relative flex w-full items-center justify-center h-screen">
 				<Image
-					className="absolute left-[-5%] md:left-0 top-[10%] md:top-[-12%] -z-10 h-full md:h-[125%] select-none object-contain object-left"
+					className="absolute left-[-5%] md:left-0 my-auto -z-30 w-1/2 md:w-auto md:h-[125%] select-none object-contain object-left"
 					draggable={false}
 					alt=""
 					src={splashImgLeftUni}
 				/>
-				<div className="flex h-full w-full select-none flex-col items-center justify-start">
+				<div className="flex h-full w-full select-none flex-col items-center justify-center">
 					<Image
 						className="-ml-[8%] mt-[0%] w-full md:ml-0 md:w-3/4"
 						draggable={false}
@@ -134,39 +134,19 @@ const Home: NextPage = () => {
 					</div>
 				</div>
 				<Image
-					className="absolute right-[-5%] md:right-0 top-[10%] md:top-[-12%] -z-10 h-full md:h-[125%] select-none object-contain object-right"
+					className="absolute right-[-5%] md:right-0 my-auto -z-30 w-1/2 md:w-auto md:h-[125%] select-none object-contain object-right"
 					draggable={false}
 					alt=""
 					src={splashImgRightUni}
 				/>
 			</div>
-			<div className="relative flex aspect-[7/8] h-full w-full flex-col items-center justify-center overflow-visible overflow-x-clip sm:aspect-video">
-				<Image
-					className="absolute -top-[22%] left-0 -z-20 h-1/2 w-full rotate-180 object-contain sm:-top-[25%]"
-					draggable={false}
-					alt=""
-					src={blueUniverse}
-				/>
-				<motion.p
-					className="absolute top-[17%] left-4 z-10 w-1/2 text-left text-[8px] sm:top-[10%] sm:left-[10%] sm:w-[30%] md:text-center md:text-xs lg:text-base"
-					style={{ y: useTransform(scrollYProgress, [0, 1], ["0%", "100%"]) }}
-				>
-					The E-Summit is the flagship event of E-Cell, which provides a
-					platform to listen to and connect with industry stalwarts, new
-					entrepreneurs, and provides a platform to try, fail, learn and test
-					out your ideas in front of a huge audience.
-				</motion.p>
-				<motion.div
-					className="absolute top-[12%] -right-1/2 -z-10 w-[95%]"
-					style={{ x: useTransform(scrollYProgress, [0, 1], ["0%", "-75%"]) }}
-				>
-					<Image
-						className="w-full object-contain"
-						draggable={false}
-						alt=""
-						src={imagination}
-					/>
-				</motion.div>
+			<Image
+				className="absolute right-0 left-0 top-[70vh] sm:top-[50vh] mx-auto -z-20 w-full sm:w-[750px] rotate-180 object-contain mix-blend-lighten"
+				draggable={false}
+				alt=""
+				src={blueUniverse}
+			/>
+			<div className="relative flex h-[85vh] sm:h-screen w-full flex-col items-center justify-center overflow-visible overflow-x-clip">
 				{video && (
 					<button
 						className="fixed top-0 left-0 z-50 h-full w-full backdrop-blur-lg"
@@ -175,72 +155,95 @@ const Home: NextPage = () => {
 						}}
 					/>
 				)}
-				<div
-					className="relative mx-4 overflow-hidden rounded-2xl sm:w-1/2 md:rounded-[50px]"
-					ref={videoRef}
-					style={{
-						transform: video && md ? "scale(1.5)" : "scale(1)",
-						transition: "transform 0.8s",
-						zIndex: video ? 100 : 0,
-					}}
-				>
-					<button
-						onClick={() => {
-							setVideo((v: boolean) => !v);
-						}}
+				<div className="relative sm:w-[60%]">
+					<motion.p
+						className="absolute z-10 top-[-20%] lg:top-[-30%] left-1 sm:left-[-30%] w-fit text-left text-[8px] md:text-center md:text-xs lg:text-base"
+						style={{ y: useTransform(scrollYProgress, [0, 1], ["0%", "100%"]) }}
+					>
+						The E-Summit is the flagship event of E-Cell, which provides a <br />
+						platform to listen to and connect with industry stalwarts, new <br />
+						entrepreneurs, and provides a platform to try, fail, learn and test <br />
+						out your ideas in front of a huge audience.
+					</motion.p>
+					<motion.div
+						className="absolute top-[-25%] -right-[150%] -z-10 w-[190%]"
+						style={{ x: useTransform(scrollYProgress, [0, 1], ["0%", "-75%"]) }}
 					>
 						<Image
 							className="w-full object-contain"
 							draggable={false}
 							alt=""
-							src={videoThumbnail}
+							src={imagination}
 						/>
-						<div className="absolute bottom-0 left-0 h-1/6 w-full bg-gradient-to-t from-black to-transparent" />
+					</motion.div>
+					<div
+						className="relative mx-4 overflow-hidden rounded-2xl md:rounded-[50px]"
+						ref={videoRef}
+						style={{
+							transform: video && md ? "scale(1.5)" : "scale(1)",
+							transition: "transform 0.8s",
+							zIndex: video ? 100 : 0,
+						}}
+					>
+						<button
+							onClick={() => {
+								setVideo((v: boolean) => !v);
+							}}
+						>
+							<Image
+								className="w-full object-contain"
+								draggable={false}
+								alt=""
+								src={videoThumbnail}
+							/>
+							<div className="absolute bottom-0 left-0 h-1/6 w-full bg-gradient-to-t from-black to-transparent" />
+							<Image
+								className="absolute bottom-0 left-0 w-1/5"
+								draggable={false}
+								alt=""
+								src={playButton}
+							/>
+							{video && (
+								<iframe
+									className="absolute top-0 left-0 h-full w-full"
+									src="https://www.youtube.com/embed/pR0sQaWS5oc?autoplay=1"
+									title="YouTube video player"
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media;"
+									allowFullScreen
+								/>
+							)}
+						</button>
+					</div>
+					<motion.p
+						className="absolute z-10 bottom-[-20%] lg:bottom-[-30%] right-1 sm:right-[-30%] w-fit text-right text-[8px] md:text-center md:text-xs lg:text-base"
+						style={{ y: useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]) }}
+					>
+						The E-Summit covers four broad verticals, namely, <br />
+						Med-Tech, Meta, Social Media and Electric Mobility. We <br />
+						strongly believe that these four are the pillars for the<br />
+						growth of entrepreneurship in the near
+						future.
+					</motion.p>
+					<motion.div
+						className="absolute bottom-[-25%] -left-[150%] -z-10 w-[190%]"
+						style={{ x: useTransform(scrollYProgress, [0, 1], ["0%", "75%"]) }}
+					>
 						<Image
-							className="absolute bottom-0 left-0 w-1/5"
+							className="w-full object-contain"
 							draggable={false}
 							alt=""
-							src={playButton}
+							src={innovation}
 						/>
-						{video && (
-							<iframe
-								className="absolute top-0 left-0 h-full w-full"
-								src="https://www.youtube.com/embed/pR0sQaWS5oc?autoplay=1"
-								title="YouTube video player"
-								allow="accelerometer; autoplay; clipboard-write; encrypted-media;"
-								allowFullScreen
-							/>
-						)}
-					</button>
+					</motion.div>
 				</div>
-				<motion.p
-					className="absolute bottom-[17%] right-4 z-10 w-1/2 text-right text-[8px] sm:bottom-[10%] sm:right-[10%] sm:w-[30%] md:text-center md:text-xs lg:text-base"
-					style={{ y: useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]) }}
-				>
-					The E-Summit covers four broad verticals, namely, Med-Tech, Meta,
-					Social Media and Electric Mobility. We strongly believe that these
-					four are the pillars for the growth of entrepreneurship in the near
-					future.
-				</motion.p>
-				<motion.div
-					className="absolute bottom-[12%] -left-1/2 -z-10 w-[95%]"
-					style={{ x: useTransform(scrollYProgress, [0, 1], ["0%", "75%"]) }}
-				>
-					<Image
-						className="w-full object-contain"
-						draggable={false}
-						alt=""
-						src={innovation}
-					/>
-				</motion.div>
-				<Image
-					className="absolute -bottom-[10%] left-0 -z-20 h-1/2 w-full object-contain"
-					draggable={false}
-					alt=""
-					src={blueUniverse}
-				/>
 			</div>
-			<div className="relative flex w-full flex-col items-center justify-center md:gap-[20px]">
+			<Image
+				className="absolute right-0 left-0 top-[160vh] sm:top-[150vh] mx-auto -z-20 w-full sm:w-[750px] rotate-180 object-contain mix-blend-lighten"
+				draggable={false}
+				alt=""
+				src={blueUniverse}
+			/>
+			<div className="relative flex h-screen w-full flex-col items-center justify-center md:gap-[20px]">
 				<p className="text-center text-[1.5rem] text-white md:text-[50px]">
 					Become the part of
 					<br /> these amazing <b>Events</b>
@@ -263,7 +266,7 @@ const Home: NextPage = () => {
 							<a key={index} href={data_item[2]} rel="noreferrer">
 								<div
 									className={`relative flex aspect-video h-[125px] shrink-0 grow-0 items-end overflow-hidden rounded-md bg-black md:h-[300px]
-											${selectedIndex === index ? "z-10 animate-scale" : ""}
+											${selectedIndex === index ? "z-10 animate-scale" : "backdrop-blur-sm"}
 										`}
 								>
 									<Image
