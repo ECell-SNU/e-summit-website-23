040034192fe5247500ff9440285867af0736da6a
Somesh Kar <somesh.kar@gmail.com>
2023-01-21 03:43:15 +0530

    Setup teamCheckout mutation

--- src/hooks/useCheckout.ts
+++ src/hooks/useCheckout.ts
@@ -2,6 +2,8 @@ import { Gender } from "@prisma/client";
 import { useState, type Dispatch, type SetStateAction } from "react";
 import { z } from "zod";
 
+import { trpc } from "../utils/trpc";
+
 export type Member = {
   name: string;
   phoneNumber: string;
@@ -31,26 +33,39 @@ export interface CheckoutState {
 }
 
 const useCheckout = () => {
+  const teamCheckoutMutation =
+    trpc.teamCheckout.handleInitialCheckout.useMutation();
+
   // TODO: get currently added members from DB via trpc and populate this
   const [members, setMembers] = useState<Member[] | any>([
-    { name: "Somesh Kar" },
-    { name: "Pratham Aggarwal" },
-    { name: "Prabhav Pandey" },
+    { name: "Somesh Kar", gender: "MALE" },
+    { name: "Pratham Aggarwal", gender: "MALE" },
+    { name: "Prabhav Pandey", gender: "MALE" },
   ]);
+
   const [checkoutState, setCheckoutState] = useState<TeamCheckoutState>(
     TeamCheckoutState.MEMBER_INFO
   );
   // -1 means new member, others mean editing some member
   const [isActive, setIsActive] = useState(-1);
-  const addMember = (member: Member) => setMembers([...members, member]);
+  const addMember = (member: Member) => {
+    setMembers([...members, member]);
 
-  const removeMember = (index: number) =>
+    teamCheckoutMutation.mutate({ teamName: "some team", members });
+  };
+
+  const removeMember = (index: number) => {
     setMembers(members.filter((_: any, i: number) => i !== index));
+
+    teamCheckoutMutation.mutate({ teamName: "some team", members });
+  };
 
   const editMember = (member: Member, index: number) => {
     const newMembers = [...members];
     newMembers[index] = member;
     setMembers(newMembers);
+
+    teamCheckoutMutation.mutate({ teamName: "some team", members });
   };
 
   return {
